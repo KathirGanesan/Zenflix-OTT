e32547cde428102114b4270163b21b994d832c74
Kathir <kathirfile@gmail.com>
2025-01-06 16:34:41 +0530

    Implemented Validation for fields. Get Videos by Genre API. Minor changes in Authorization. Code optimization. Implemented env file.

---
+++ src/main/java/com/zenflix/ott/config/DotenvEnvironmentPostProcessor.java
@@ -0,0 +1,18 @@
+package com.zenflix.ott.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+public class DotenvEnvironmentPostProcessor implements EnvironmentPostProcessor {
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> {
+            // Let Spring see them as system properties
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+    }
+}
