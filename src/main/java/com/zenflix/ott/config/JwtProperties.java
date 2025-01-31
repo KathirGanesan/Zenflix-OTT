fe7e881ddc13b4e847d477a66a1c1ad786ce3498
Kathir <kathirfile@gmail.com>
2025-01-31 19:38:21 +0530

    Handled JWT config using ConfigurationProperties

--- src/main/java/com/zenflix/ott/config/JwtProperties.java
+++ src/main/java/com/zenflix/ott/config/JwtProperties.java
@@ -1,4 +1,22 @@
 package com.zenflix.ott.config;
 
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
+    private String secret;
+
+    @PostConstruct
+    public void validateConfig() {
+        if (secret == null || secret.isEmpty() ) {
+            throw new IllegalStateException("JWT Secret key is missing. Check your configuration.");
+        }
+    }
 }
